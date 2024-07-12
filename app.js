2714346f7060e7044a4a81b2e1209f997fb07f41
Erick <erick.nahuelpaiva.001@gmail.com>
2024-07-12 20:25:14 -0300

    agregado uso de nodemon y rutas iniciales

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ import express from "express";
 
 const app = express();
 
+app.get("/inicio",(req, res)=>{
+    res.send("<h1>Inicio</h1>");
+});
+
+
+app.use((req,res)=>{
+    res.send("<h1>Ruta no encontrada</h1>");
+    /* res.status(404).json({mensaje: "error: ruta no encontrada"}); */
+});
+
+
 app.listen(3000, ()=>{
     console.log("servidor activo");
 });
